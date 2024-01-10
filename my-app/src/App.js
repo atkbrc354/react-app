5b9951d3bff4cdb4eeeb758f655b9ea8f3b92cc6
djalil2004 <122861717+djalil2004@users.noreply.github.com>
2024-01-10 17:25:00 +0300

    fix: App,js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './Components/Header';
 
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Header />
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
