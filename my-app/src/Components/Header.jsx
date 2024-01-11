2a8f88155fa4288a4f19af930e8b47c12703fd5e
Rinat <atkbrc354@mail.ru>
2024-01-11 17:28:23 +0300

    feah: add header-top

--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -4,18 +4,23 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function Header() {
     return (
-      <div className={styleHeader.header}>
-        <Router>
-          <nav>
-              <ul className={ styleHeader.list }>
-                  <li className={ styleHeader.item }><Link className={ styleHeader.link} to="#">Отзывы</Link></li>
-                  <li><Link className={ styleHeader.link} to="#">Гарантии</Link></li>
-                  <li><Link className={ styleHeader.link} to="#">Как купить</Link></li>
-                  <li><Link className={ styleHeader.link} to="#">Накопительная</Link></li>
-                  <li><Link className={ styleHeader.link} to="#">Заработай</Link></li>
-              </ul>
-          </nav>
-        </Router>
+      <div className={styleHeader.wrapper}>
+        <div className={styleHeader.header}>
+          <p className={ styleHeader.interpreter}>RU/$</p>
+          <p className={ styleHeader.header_desc}>Накопительный счет</p>
+          <Router>
+            <nav className={ styleHeader.header_nav }>
+                <ul className={ styleHeader.list }>
+                    <li className={ styleHeader.item }><Link className={ styleHeader.link} to="#">Отзывы</Link></li>
+                    <li><Link className={ styleHeader.link} to="#">Гарантии</Link></li>
+                    <li><Link className={ styleHeader.link} to="#">Как купить</Link></li>
+                    <li><Link className={ styleHeader.link} to="#">Накопительная</Link></li>
+                    <li><Link className={ styleHeader.link} to="#">Заработай</Link></li>
+                </ul>
+            </nav>
+          </Router>
+          <p className={ styleHeader.profile }>Hanzed Rules</p>
+        </div>
       </div>
     );
   }
