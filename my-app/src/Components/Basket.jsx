916da5e20cf3c4e997101ddc4157c28374e6e545
Rinat <atkbrc354@mail.ru>
2024-01-15 11:03:27 +0300

    feah: addet basket_box_left

--- my-app/src/Components/Basket.jsx
+++ my-app/src/Components/Basket.jsx
@@ -1,10 +1,23 @@
-import styleHeader from '../scss/header.module.scss';
+import styleBasket from '../scss/bascket.module.scss';
 
 
 function Basket() {
     return (
-        <div>
-            <h1>Корзина</h1>
+        <div className='wrapper'>
+            <h1 className={styleBasket.basket_title}>Корзина <span>5</span></h1>
+            <div className={styleBasket.basket_box}>
+                <div className={styleBasket.basket_box_left}>
+                    <div className={styleBasket.basket_box_top}>
+                        <h2 className={styleBasket.basket_box_title}>Войдите или зарегистрируйтесь</h2>
+                        <p className={styleBasket.basket_box_desc}>Вы сможете накапливать бонусные рубли
+                        и использовать их при оплате до 50% стоимости товаров.</p>
+                        <button className={styleBasket.basket_box_btn}>Войти</button>
+                    </div>
+                </div>
+                <div className={styleBasket.basket_box_rigth}>
+
+                </div>
+            </div>
         </div>
     )
 }
